6146d40f4c6675fd14f5563e45395e2cc7378efc
Your Actual Name <Your Actual Email>
2017-02-11 13:27:08 +0100

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, worl!"
+    render html: "hello, world!"
   end
 end
