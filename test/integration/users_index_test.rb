7c9aacc3ce53d20a68b062a8a426a67fffed0da5
Your Actual Name <Your Actual Email>
2017-02-20 10:36:31 +0100

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,7 +7,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test " index as admin including pagination and delete links" do
+  test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
